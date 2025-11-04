937df39a0a9ccc230291ff82a63f07368755267c
Abdullah Al-Hasani <arzero.24@gmail.com>
2025-11-04 15:24:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js"></script>
+    <title>BooxCodex</title>
+</head>
+
+<body>
+
+    <header>
+        <img src="favicon.svg" alt="logo.svg" height="60px" style="vertical-align: middle">
+        <h1 style="display: inline; vertical-align: middle;">BooxCodex</h1>
+    </header>
+
+    <?php
+        /*
+            PHP & Database Requirements:
+            ▪ MySQL with 15+ records
+            ▪ At least 3 fields per item (id, name, description)
+            ▪ Support insert, update, delete functionality.
+            ▪ Display data on dashboard.php
+            ▪ Simple search function
+         */
+    ?>
+
+    <footer id="legal">
+        <h6>
+            G4 Web Development Demo
+        </h6>
+    </footer>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+    JavaScript Requirements:
+    ▪ Display item info on click
+    ▪ Form validation (required fields, email format)
+    ▪ Slideshow for images or simple animation
+*/
